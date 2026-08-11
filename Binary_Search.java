61b7d4160ccca7847850249ef95f882246853a09
Aksarkar33 <aksarkar323@gmail.com>
2026-08-12 02:34:13 +0530

    Add Binary Search - new pattern (divide and conquer on sorted array)

---
+++ Binary_Search.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Binary_Search {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int target=sc.nextInt();
+
+        int left=0;
+        int right=arr.length-1;
+       
+        int index=-1;
+        while (left<=right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid]==target) {
+                index=mid;
+                break;
+            }
+            if (arr[mid]<target) {
+                left=mid+1;
+            }else{
+                right=mid-1;
+            }
+            
+
+        }
+        System.out.println(index);
+
+    }
+}
