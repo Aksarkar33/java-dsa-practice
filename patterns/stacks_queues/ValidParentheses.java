25cee7d8f183e643d1b967b62fe4e582edfbffce
Aksarkar33 <aksarkar323@gmail.com>
2026-09-01 12:46:04 +0530

    Add Stacks/Queues: Valid Parentheses

--- patterns/stacks_queues/ValidParentheses.java
+++ patterns/stacks_queues/ValidParentheses.java
@@ -32,9 +32,9 @@ public class ValidParentheses {
     }
 
     public static void main(String[] args) {
-        System.out.println(isValid("()[]{}")); // expected: true
-        System.out.println(isValid("(]")); // expected: false
-        System.out.println(isValid("([)]")); // expected: false
-        System.out.println(isValid("{[]}")); // expected: true
+        System.out.println(isValid("()[]{}"));
+        System.out.println(isValid("(]")); 
+        System.out.println(isValid("([)]")); 
+        System.out.println(isValid("{[]}")); 
     }
 }
